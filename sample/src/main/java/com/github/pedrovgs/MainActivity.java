ff8f20cc36ba0fabf9723b65206dc3d5c723651c
pedrovgs <pedrovicente.gomez@gmail.com>
2014-03-05 22:16:30 +0100

    Create repository with basic structure.

---
+++ sample/src/main/java/com/github/pedrovgs/MainActivity.java
@@ -0,0 +1,16 @@
+package com.github.pedrovgs;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author Pedro Vicente Gómez Sánchez.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
