845f11cbc55fd51f8a244dcf975e57bba91ce07b
pedrovgs <pedrovicente.gomez@gmail.com>
2014-03-13 22:47:09 +0100

    First approach with horizontal drag working in the sample project.

---
+++ sample/src/main/java/com/github/pedrovgs/DraggableView.java
@@ -0,0 +1,112 @@
+package com.github.pedrovgs;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.support.v4.view.MotionEventCompat;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.widget.ViewDragHelper;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
+
+/**
+ * Class created to extends a ViewGroup and simulate the YoutubeLayoutComponent
+ *
+ * @author "Pedro Vicente Gómez Sánchez"
+ */
+public class DraggableView extends LinearLayout {
+
+    /*
+     * Constants
+     */
+    private static final String LOGTAG = "DraggableView";
+
+
+    /*
+     * Attributes
+     */
+
+    private ViewDragHelper dragHelper;
+    private View dragView;
+
+
+    /*
+     * Constructors
+     */
+
+    public DraggableView(Context context) {
+        super(context);
+        initializeView();
+    }
+
+    public DraggableView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initializeView();
+    }
+
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    public DraggableView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        initializeView();
+    }
+
+    private void initializeView() {
+        dragHelper = ViewDragHelper.create(this, 1f, new DragHelperCallback());
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        dragView = findViewById(R.id.dragView);
+    }
+
+    @Override
+    public void computeScroll() {
+        if (dragHelper.continueSettling(true)) {
+            ViewCompat.postInvalidateOnAnimation(this);
+        }
+    }
+
+    /*
+     * DragHelperCallback
+     */
+
+    private class DragHelperCallback extends ViewDragHelper.Callback {
+        @Override
+        public void onViewPositionChanged(View changedView, int left, int top, int dx, int dy) {
+            invalidate();
+        }
+
+        @Override
+        public boolean tryCaptureView(View view, int pointerId) {
+            return view == dragView;
+        }
+
+        @Override
+        public int clampViewPositionHorizontal(View child, int left, int dx) {
+            return left;
+        }
+
+    }
+
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        final int action = MotionEventCompat.getActionMasked(ev);
+        if (action == MotionEvent.ACTION_CANCEL || action == MotionEvent.ACTION_UP) {
+            dragHelper.cancel();
+            return false;
+        }
+        return dragHelper.shouldInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        dragHelper.processTouchEvent(ev);
+        return true;
+    }
+
+}
--- sample/src/main/java/com/github/pedrovgs/MainActivity.java
+++ sample/src/main/java/com/github/pedrovgs/MainActivity.java
@@ -2,15 +2,19 @@ package com.github.pedrovgs;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * @author Pedro Vicente Gómez Sánchez.
  */
 public class MainActivity extends Activity {
 
+    private View header;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
