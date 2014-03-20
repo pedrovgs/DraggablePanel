d86e5c7f2b3e5c5b1a58c7ace8074daee428a9fe
pedrovgs <pedrovicente.gomez@gmail.com>
2014-03-20 21:10:27 +0100

    First iteration over Youtube sample

--- draggablepanel/src/main/java/com/github/pedrovgs/DraggablePanel.java
+++ draggablepanel/src/main/java/com/github/pedrovgs/DraggablePanel.java
@@ -94,8 +94,10 @@ public class DraggablePanel extends FrameLayout {
         draggableView.setTopViewHeight(topFragmentHeight);
         draggableView.setFragmentManager(fragmentManager);
         draggableView.attachTopFragment(topFragment);
+
         draggableView.setBackground(getBackground());
         setBackground(null);
+
         draggableView.setScaleFactor(scaleFactor);
         draggableView.setTopFragmentMarginRight(topFragmentMarginRight);
         draggableView.setTopFragmentMarginBottom(topFragmentMarginBottom);
--- sample/src/main/java/com/github/pedrovgs/sample/activity/YoutubeSampleActivity.java
+++ sample/src/main/java/com/github/pedrovgs/sample/activity/YoutubeSampleActivity.java
@@ -5,20 +5,67 @@ import android.support.v4.app.FragmentActivity;
 import android.widget.ListView;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import com.github.pedrovgs.DraggablePanel;
 import com.github.pedrovgs.sample.R;
+import com.github.pedrovgs.sample.fragment.RedFragment;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerSupportFragment;
 
 /**
  * @author Pedro Vicente Gómez Sánchez.
  */
 public class YoutubeSampleActivity extends FragmentActivity {
 
+    private static final String YOUTUBE_API_KEY = "AIzaSyD2MyIMbskwhmVSNcZQf9yBWcYo-dWPZOo";
+    private static final String VIDEO_KEY = "PpTGsZTMXlY";
+
     @InjectView(R.id.lv_videos)
     ListView lv_videos;
+    @InjectView(R.id.draggable_panel)
+    DraggablePanel draggablePanel;
+
+    private YouTubePlayer youtubePlayer;
+
+    private YouTubePlayerSupportFragment youtubeFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_youtube_sample);
         ButterKnife.inject(this);
+        initializeYoutubeFragment();
+        initializeDraggablePanel();
+    }
+
+    private void initializeYoutubeFragment() {
+        youtubeFragment = new YouTubePlayerSupportFragment();
+        youtubeFragment.initialize(YOUTUBE_API_KEY,
+                new YouTubePlayer.OnInitializedListener() {
+
+                    @Override
+                    public void onInitializationSuccess(YouTubePlayer.Provider provider,
+                                                        YouTubePlayer player, boolean wasRestored) {
+                        if (!wasRestored) {
+                            youtubePlayer = player;
+                            youtubePlayer.loadVideo(VIDEO_KEY);
+                            youtubePlayer.setShowFullscreenButton(true);
+                        }
+                    }
+
+                    @Override
+                    public void onInitializationFailure(YouTubePlayer.Provider provider,
+                                                        YouTubeInitializationResult error) {
+                    }
+
+                }
+        );
+    }
+
+    private void initializeDraggablePanel() {
+        draggablePanel.setFragmentManager(getSupportFragmentManager());
+        draggablePanel.setTopFragment(youtubeFragment);
+        draggablePanel.setBottomFragment(new RedFragment());
+        draggablePanel.initializeView();
     }
 }
