59c96ae2e6196ed6414ab62c00e9378a46614b14
pedrovgs <pedrovicente.gomez@gmail.com>
2014-03-20 22:20:46 +0100

    Finish first iteration over video sample

--- sample/src/main/java/com/github/pedrovgs/sample/activity/VideoSampleActivity.java
+++ sample/src/main/java/com/github/pedrovgs/sample/activity/VideoSampleActivity.java
@@ -1,9 +1,11 @@
 package com.github.pedrovgs.sample.activity;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.MediaController;
 import android.widget.VideoView;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -15,6 +17,8 @@ import com.github.pedrovgs.sample.R;
  */
 public class VideoSampleActivity extends FragmentActivity {
 
+    public static final String APPLICATION_RAW_PATH = "android.resource://com.github.pedrovgs.sample/";
+    
     @InjectView(R.id.lv_videos)
     ListView lv_videos;
     @InjectView(R.id.draggable_view)
@@ -29,6 +33,12 @@ public class VideoSampleActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_sample);
         ButterKnife.inject(this);
+        Uri path = Uri.parse(APPLICATION_RAW_PATH + R.raw.video);
+        MediaController mediaController = new MediaController(this);
+        video_view.setMediaController(mediaController);
+        video_view.setVideoURI(path);
+        video_view.start();
+
     }
 
 
