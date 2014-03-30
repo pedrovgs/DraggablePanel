fca1b62450243493b652828c6e3e0d557ac2bf52
pedrovgs <pedrovicente.gomez@gmail.com>
2014-03-30 12:44:40 +0200

    Start working with DraggableView in tv shows sample activity.

--- sample/src/main/java/com/github/pedrovgs/sample/activity/TvShowsActivity.java
+++ sample/src/main/java/com/github/pedrovgs/sample/activity/TvShowsActivity.java
@@ -1,12 +1,18 @@
 package com.github.pedrovgs.sample.activity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.ImageView;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import com.github.pedrovgs.DraggableView;
 import com.github.pedrovgs.sample.R;
 import com.github.pedrovgs.sample.viewmodel.TvShowViewModel;
 import com.pedrogomez.renderers.RendererAdapter;
+import com.squareup.picasso.Picasso;
 
 import javax.inject.Inject;
 
@@ -20,11 +26,41 @@ public class TvShowsActivity extends DIFragmentActivity {
 
     @InjectView(R.id.gv_tv_shows)
     GridView gv_tv_shows;
+    @InjectView(R.id.iv_poster)
+    ImageView iv_poster;
+    @InjectView(R.id.draggable_view)
+    DraggableView draggableView;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tv_shows_sample);
         ButterKnife.inject(this);
-        gv_tv_shows.setAdapter(adapter);
+        initializeDraggableView();
+        initializeGridView();
     }
+
+    private void initializeDraggableView() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                draggableView.setVisibility(View.GONE);
+                draggableView.minimize();
+            }
+        }, 10);
+    }
+
+    private void initializeGridView() {
+        gv_tv_shows.setAdapter(adapter);
+        gv_tv_shows.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                TvShowViewModel tvShow = adapter.getItem(position);
+                Picasso.with(getBaseContext()).load(tvShow.getThumbnail()).placeholder(R.drawable.tv_show_placeholder).into(iv_poster);
+                draggableView.setVisibility(View.VISIBLE);
+                draggableView.maximize();
+            }
+        });
+    }
+
 }
