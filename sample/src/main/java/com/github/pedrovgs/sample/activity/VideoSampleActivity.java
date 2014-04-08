89b06ade9b7ab5076253e601dead9a1a807aeaab
pedrovgs <pedrovicente.gomez@gmail.com>
2014-04-08 21:02:43 +0200

    Improve VideoSampleActivity with DraggableViewListeners to pause/resume the video

--- sample/src/main/java/com/github/pedrovgs/sample/activity/VideoSampleActivity.java
+++ sample/src/main/java/com/github/pedrovgs/sample/activity/VideoSampleActivity.java
@@ -10,6 +10,7 @@ import android.widget.VideoView;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
+import com.github.pedrovgs.DraggableListener;
 import com.github.pedrovgs.DraggableView;
 import com.github.pedrovgs.sample.R;
 import com.squareup.picasso.Picasso;
@@ -24,9 +25,9 @@ public class VideoSampleActivity extends FragmentActivity {
     private static final String VIDEO_THUMBNAIL = "http://wac.450f.edgecastcdn.net/80450F/screencrush.com/files/2013/11/the-amazing-spider-man-2-poster-green-goblin.jpg";
 
     @InjectView(R.id.draggable_view)
-    DraggableView draggable_view;
+    DraggableView draggableView;
     @InjectView(R.id.video_view)
-    VideoView video_view;
+    VideoView videoView;
     @InjectView(R.id.iv_thumbnail)
     ImageView iv_thumbnail;
     @InjectView(R.id.iv_poster)
@@ -39,14 +40,45 @@ public class VideoSampleActivity extends FragmentActivity {
         ButterKnife.inject(this);
         initializeVideoView();
         initializePoster();
+        hookDraggableViewListener();
+    }
+
+    private void hookDraggableViewListener() {
+        draggableView.setDraggableListener(new DraggableListener() {
+            @Override
+            public void onMaximized() {
+                if (!videoView.isPlaying()) {
+                    videoView.start();
+                }
+            }
+
+            @Override
+            public void onMinimized() {
+
+            }
+
+            @Override
+            public void onClosedToLeft() {
+                if (videoView.isPlaying()) {
+                    videoView.pause();
+                }
+            }
+
+            @Override
+            public void onClosedToRight() {
+                if (videoView.isPlaying()) {
+                    videoView.pause();
+                }
+            }
+        });
     }
 
     private void initializeVideoView() {
         Uri path = Uri.parse(APPLICATION_RAW_PATH + R.raw.video);
         MediaController mediaController = new MediaController(this);
-        video_view.setMediaController(mediaController);
-        video_view.setVideoURI(path);
-        video_view.start();
+        videoView.setMediaController(mediaController);
+        videoView.setVideoURI(path);
+        videoView.start();
     }
 
     private void initializePoster() {
@@ -66,6 +98,6 @@ public class VideoSampleActivity extends FragmentActivity {
 
     @OnClick(R.id.iv_poster)
     void onPosterClicked() {
-        draggable_view.maximize();
+        draggableView.maximize();
     }
 }
