f531f7f0fe219797e5430a88e2488b2c3f4a6a15
pedrovgs <pedrovicente.gomez@gmail.com>
2014-04-12 18:45:39 +0200

    Add javadoc to DraggableListener

--- draggablepanel/src/main/java/com/github/pedrovgs/DraggableListener.java
+++ draggablepanel/src/main/java/com/github/pedrovgs/DraggableListener.java
@@ -16,16 +16,30 @@
 package com.github.pedrovgs;
 
 /**
+ * Listener created to be notified when some drag actions are performed over DraggablePanel or DraggableView instances.
+ *
  * @author Pedro Vicente Gómez Sánchez.
  */
 public interface DraggableListener {
 
+    /**
+     * Called when the view is maximized.
+     */
     void onMaximized();
 
+    /**
+     * Called when the view is minimized.
+     */
     void onMinimized();
 
+    /**
+     * Called when the view is closed to the left.
+     */
     void onClosedToLeft();
 
+    /**
+     * Called when the view is closed to the right.
+     */
     void onClosedToRight();
 
 }
